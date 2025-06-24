e0e1b3fc003147df4462247967cd75ea8b4132b9
DESKTOP-HLST3C8 <lybyte@vivaldi.net>
2025-06-24 16:25:30 +0900

    250624

---
+++ Flutter_Projects/sample/lib/file.dart
@@ -0,0 +1,91 @@
+ import 'package:flutter/material.dart';
+ import 'package:flutter/services.dart';
+ import 'dart:ui' as ui;
+
+void main()=> runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Generated App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xff2196f3),
+        canvasColor: const Color(0xfffafafa),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _controller = TextEditingController();
+  final _fname = 'assets/documents/data.txt';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: <Widget>[
+              Text('RESOURCE ACCESS.',
+                style: TextStyle(fontSize: 32,
+                  fontWeight: ui.FontWeight.w500),
+              ),
+              Padding(padding: EdgeInsets.all(10.0)),
+              TextField(
+                controller: _controller,
+                style: TextStyle(fontSize: 24,),
+                minLines: 1,
+                maxLines: 5,
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.open_in_new),
+          onPressed: () async {
+            final value = await loadIt();
+            setState(() {
+              _controller.text = value;
+            });
+            showDialog(
+              context: context,
+              builder: (BuildContext context) =>
+                AlertDialog(
+                  title: Text("loaded!"),
+                  content: Text("load message from Asset."),
+                )
+            );
+          }
+        )
+    );
+  }
+
+  Future<String> getDataAsset(String path) async {
+    return await rootBundle.loadString(path);
+  }
+
+  Future<String> loadIt() async {
+    try {
+      final res = await getDataAsset(_fname);
+      return res;
+    } catch (e) {
+      return '*** no data ***';
+    }
+  }
+}
+
