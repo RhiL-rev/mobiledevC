f3aca3a427770604c9e577bb41ef9fcbdac9dd7b
DESKTOP-HLST3C8 <lybyte@vivaldi.net>
2025-07-01 16:28:41 +0900

    0701 commit

---
+++ Flutter_Projects/sample/lib/firestoreAccess.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+import 'dart:convert';
+import 'dart:io';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Generated App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xff2196f3),
+        canvasColor: const Color(0xfffafafa),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _controller = TextEditingController();
+  static const host = 'baconipsum.com';
+  static const path = '/api/?type=meat-and-filler&paras=1&format=text';
+  static const url = 'https://baconipsum.com/api/?type=meat-and-filler&paras=1&format=text';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            Text('INTERNET ACCESS.',
+              style: TextStyle(fontSize: 32,
+                  fontWeight: ui.FontWeight.w500),
+            ),
+            Padding(padding: EdgeInsets.all(10.0)),
+            TextField(
+              controller: _controller,
+              style: TextStyle(fontSize: 24,),
+              minLines: 1,
+              maxLines: 5,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.open_in_new),
+        onPressed: () {
+          fire();
+          showDialog(
+              context: context,
+              builder: (BuildContext context) => AlertDialog(
+                title: Text("loaded!"),
+                content: Text("get content from URI."),
+              )
+          );
+        },
+      ),
+    );
+  }
+
+  void getData() async {
+    var https = await HttpClient();
+    HttpClientRequest request = await https.getUrl(Uri.parse(url));
+    HttpClientResponse response = await request.close();
+    final value = await response.transform(utf8.decoder).join();
+    _controller.text = value;
+  }
+
+  void fire() async {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final snapshot = await firestore.collection('mydata').get();
+  var msg = '';
+  snapshot.docChanges.forEach((element) {
+    final name = element.doc.get('name');
+    final mail = element.doc.get('mail');
+    final age = element.doc.get('age');
+    msg += "${name} (${age}) <${mail}>\n";
+  });
+  _controller.text = msg;
+  }
+
+}
